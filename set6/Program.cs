afaa130899dfbe64fa52db01592a61a8b24e8c9c
Konstantin Smirnov <sasharugby@yandex.ru>
2024-08-12 18:44:32 +0300

    122done

---
+++ set6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+public class ClientTests
+{
+   
+    public void CanConnectToServer()
+    {
+        // Arrange
+        Client client = new Client();
+
+        // Act
+        bool isConnected = client.ConnectToServer("192.168.1.1");
+
+        // Assert
+        Assert.True(isConnected);
+    }
+
+    
+    public void CanSendDataToServer()
+    {
+        // Arrange
+        Client client = new Client();
+
+        // Act
+        bool isDataSent = client.SendDataToServer("Hello, server!");
+
+        // Assert
+        Assert.True(isDataSent);
+    }
+}
+
+public class Client
+{
+    public bool ConnectToServer(string serverIP)
+    {
+        // Реализация подключения к серверу
+        return true;
+    }
+
+    public bool SendDataToServer(string data)
+    {
+        // Реализация отправки данных на сервер
+        return true;
+    }
+}
